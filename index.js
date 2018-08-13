c604286167518690674ef89f3bc5d5b8551a8b82
Ki Ki  Bush <donyabush@gmail.com>
2018-08-13 23:46:53 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,7 @@
+function previewFirstName(event, id) {
+document.getElementById('fid + ').innerText = ""
+}
+  
+
+document.getElementById('first_name').addEventListerner('keyup', PreviewFirstName);
+document.getElementById('last_name').addEventListerner('keyup', PreviewLasttName);
